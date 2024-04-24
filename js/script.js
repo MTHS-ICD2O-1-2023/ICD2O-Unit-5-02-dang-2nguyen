4b05484e1476340bb5a1f7b73052e745896a64aa
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-24 18:16:51 +0000

    eafe

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+const randomnumber = Math.floor(Math.random() * 6) + 1
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('on-check').checked
+  
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("rightAnswer").innerHTML =
+      "Value is:  " + randomnumber
+  } else {
+    document.getElementById("rightAnswer").innerHTML =
+      "Value is: - " + randomnumber
+  }
+}
